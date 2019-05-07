18d6accd2f78a1f440c5bab29c51d6b120e50df7
Matt Bell <mappum@gmail.com>
2019-05-06 19:46:15 -0700

    Update for coins API changes, and mint unbonded coins into accounts

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ let {
   multisigAccount
 } = coins
 
-module.exports = function staking (opts = {}) {
+module.exports = function staking (tokenRoute, opts = {}) {
+  if (!tokenRoute) {
+    throw Error('Must specify token route')
+  }
+
   let unbondingPeriod = opts.unbondingPeriod || 30 * 24 * 60 * 60
 
-  // create accounts handler to use recursively for bonded/unbonded accounts
+  // create accounts handler to use recursively for bonded accounts
   let handlers = Object.assign({
     ed25519: ed25519Account,
     secp256k1: secp256k1Account,
@@ -19,59 +23,44 @@ module.exports = function staking (opts = {}) {
   return {
     initialState: {
       bonded: {},
-      unbonding: [],
-      unbonded: {}
+      unbonding: []
     },
 
-    onInput (input, tx, state, chain) {
-      // withdraw from unbonded account
-      if (input.unbonded) {
-        // check user has right to spend from account,
-        // and deduct balance/increase sequence
-        accounts.onInput(input, tx, state.unbonded, chain)
-        return
-      }
-
+    onInput (input, state, context) {
       // withdraw from bonded account (must pay into unbond queue)
-      if (input.bonded) {
-        // get substate for this validator
-        let validatorPubkey = input.validatorPubkey.toLowerCase()
-        let validator = state.bonded[validatorPubkey]
-        if (validator == null) {
-          throw Error(`No staking state for validator "${validatorPubkey}"`)
-        }
-
-        // calculate number of shares (input is denominated in coins)
-        // TODO: check that numbers don't go above Number.MAX_SAFE_INTEGER
-        let sharesPerCoin = Math.floor(1e8 * validator.shares / validator.balance)
-        let shares = Math.floor(input.amount * sharesPerCoin / 1e8)
-
-        // ensure user has right to withdraw,
-        // if so then deduct share balance and increase sequence
-        // TODO: find a cleaner API for programmatically debiting accounts
-        let modifiedInput = Object.assign({}, input, { amount: shares })
-        accounts.onInput(modifiedInput, tx, validator.accounts, chain)
-
-        // remove voting power from validator
-        chain.validators[validatorPubkey] -= input.amount
-
-        // ensure tx pays into unbond queue
-        tx.mustHaveOutput({
-          type: input.type,
-          amount: input.amount,
-          address: input.address,
-          validatorPubkey,
-          unbond: true
-        })
-
-        return
+      // get substate for this validator
+      let validatorPubkey = input.validatorPubkey.toLowerCase()
+      let validator = state.bonded[validatorPubkey]
+      if (validator == null) {
+        throw Error(`No staking state for validator "${validatorPubkey}"`)
       }
 
-      throw Error('Must withdraw from bonded or unbonded accounts')
+      // calculate number of shares (input is denominated in coins)
+      // TODO: check that numbers don't go above Number.MAX_SAFE_INTEGER
+      let sharesPerCoin = Math.floor(1e8 * validator.shares / validator.balance)
+      let shares = Math.floor(input.amount * sharesPerCoin / 1e8)
+
+      // ensure user has right to withdraw,
+      // if so then deduct share balance and increase sequence
+      // TODO: find a cleaner API for programmatically debiting accounts
+      let modifiedInput = Object.assign({}, input, { amount: shares })
+      accounts.onInput(modifiedInput, validator.accounts, context)
+
+      // remove voting power from validator
+      context.validators[validatorPubkey] -= input.amount
+
+      // ensure tx pays into unbond queue
+      context.transaction.mustHaveOutput({
+        type: input.type,
+        amount: input.amount,
+        address: input.address,
+        validatorPubkey,
+        unbond: true
+      })
     },
 
     // bond, or put into unbond queue
-    onOutput (output, tx, state, chain) {
+    onOutput (output, tx, state, context) {
       // add to unbonding queue, to be processed once period is over
       if (output.unbond) {
         // get substate for this validator
@@ -86,7 +75,7 @@ module.exports = function staking (opts = {}) {
         let shares = Math.floor(output.amount * sharesPerCoin / 1e8)
 
         let modifiedOutput = Object.assign({}, output, {
-          height: chain.height + unbondingPeriod,
+          time: context.time + unbondingPeriod,
           shares
         })
         delete modifiedOutput.amount
@@ -116,13 +105,13 @@ module.exports = function staking (opts = {}) {
         validator.shares += shares
 
         // add voting power to validator
-        let votingPower = chain.validators[validatorPubkey] || 0
-        chain.validators[validatorPubkey] = votingPower + output.amount
+        let votingPower = context.validators[validatorPubkey] || 0
+        context.validators[validatorPubkey] = votingPower + output.amount
 
         // add coins to bonded account balance
         // TODO: find a cleaner API for programmatically adding to accounts
         let modifiedOutput = Object.assign({}, output, { amount: shares })
-        accounts.onOutput(modifiedOutput, tx, validator.accounts, chain)
+        accounts.onOutput(modifiedOutput, validator.accounts, context)
 
         return
       }
@@ -130,9 +119,9 @@ module.exports = function staking (opts = {}) {
       throw Error('Must bond or pay to unbond queue')
     },
 
-    onBlock ({ bonded, unbonding, unbonded }, chain) {
+    onBlock ({ bonded, unbonding }, context) {
       // process unbond queue
-      while (unbonding[0] != null && unbonding[0].height <= chain.height) {
+      while (unbonding[0] != null && unbonding[0].time <= context.time) {
         let output = unbonding.shift()
 
         // get substate for this validator
@@ -153,7 +142,7 @@ module.exports = function staking (opts = {}) {
         modifiedOutput.amount = coins
         delete modifiedOutput.shares
         // TODO: find a cleaner API for programmatically adding to accounts
-        accounts.onOutput(modifiedOutput, null, unbonded, chain)
+        context.modules[tokenRoute].mint(modifiedOutput)
       }
     }
   }
